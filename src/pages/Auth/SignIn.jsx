51e54d4155d4815323b7dbcb8df6b07400690788
yvehogan <mz.yve3@gmail.com>
2025-07-31 09:25:06 +0100

    - set up file structure for onboarding screens -created a shared folder for similar components -implemented the input otp screens

--- src/pages/Auth/SignIn.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const SignIn = () => {
-  return (
-    <div className='w-full h-screen border-2 border-red-600'>SignIn</div>
-  )
-}
-
-export default SignIn
---
+++ src/pages/InputOtp.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import InputOtpForm from '../components/auth/InputOtpForm'
+
+const InputOtp = () => {
+  return (
+    <>
+        <InputOtpForm />
+    </>
+  )
+}
+
+export default InputOtp
