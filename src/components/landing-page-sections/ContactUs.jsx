21972e44890f4f1b262e9eec5ec7c1e8636d64d9
Golden Ekpendu <ekpendugolden1@gmail.com>
2025-08-05 15:56:56 +0100

    adjusted contact form + support component

--- src/components/landing-page-sections/ContactUs.jsx
+++ src/components/landing-page-sections/ContactUs.jsx
@@ -13,7 +13,7 @@ import { Link } from 'react-router-dom';
 const ContactUs = () => {
   return (
     <section className='w-full bg-transparent py-5 flex items-center justify-center'>
-      <div className=' relative lg:container lg:max-w-[1000px] lg:min-h-[750px] rounded-lg shadow-lg lg:ml-14 lg:my-40 '>
+      <div className=' relative lg:container lg:max-w-[1000px] lg:min-h-[750px] rounded-lg lg:shadow-lg lg:ml-14 lg:my-40 '>
         <div className='lg:absolute lg:-left-36'>
           <div className=''>
             <div className='space-y-1 lg:hidden '>
@@ -23,36 +23,36 @@ const ContactUs = () => {
               <p className='text-center text-sm'>We are here to you! How can we help you.</p>
             </div>
 
-            <form action="" className='px-6 w-full space-y-5 pt-10 justify-center items-center'>
-              <div className='flex flex-col gap-8 max-w-lg mx-auto shadow-2xl lg:hidden'>
+            <form action="" className='px-6 w-full space-y-5 pt-10 justify-center items-center lg:hidden'>
+              <div className='flex flex-col gap-8 max-w-lg mx-auto shadow-2xl overflow-hidden '>
                 <div className='px-4 flex flex-col gap-8  '>
-                  <div className='flex flex-col gap-6 pt-7 w-sm'>
+                  <div className='flex flex-col gap-6 pt-7 w-[350px] md:w-md'>
                     <div className='flex flex-col gap-2'>
                       <label htmlFor="fullName" className='font-bold text-md'>Full Name</label>
-                      <input type="text" placeholder='Your name' className='py-3.5 px-4 h-[47px] rounded-lg bg-transparent border border-gray-300 placeholder:text-gray-400' />
+                      <input type="text" placeholder='Your name' className='py-3.5 px-4 h-[47px] rounded-lg bg-transparent border border-gray-300 placeholder:text-gray-400 focus:ring focus:ring-green-300 outline-none' />
                     </div>
 
                     <div className='flex flex-col gap-2'>
                       <label htmlFor="fullName" className='font-bold text-md'>Email Or Phone Number</label>
-                      <input type="text" placeholder='Enter your email or phone number' className='py-3.5 px-4 h-[47px] rounded-lg bg-transparent border border-gray-300 placeholder:text-gray-400' />
+                      <input type="text" placeholder='Enter your email or phone number' className='py-3.5 px-4 h-[47px] rounded-lg bg-transparent border border-gray-300 placeholder:text-gray-400 focus:ring focus:ring-green-300 outline-none' />
                     </div>
 
                     <div className='flex flex-col gap-2'>
-                      <textarea name="message" id="" placeholder='Type your message here...' className='py-3.5 px-4 h-[100px] rounded-lg bg-transparent border border-gray-300 placeholder:text-gray-400'></textarea>
+                      <textarea name="message" id="" placeholder='Type your message here...' className='py-3.5 px-4 h-[100px] rounded-lg bg-transparent border border-gray-300 placeholder:text-gray-400 focus:ring focus:ring-green-300 outline-none'></textarea>
                     </div>
                   </div>
 
-                  <button type="submit" className='w-fit rounded-lg text-md lg:text-2xl text-white bg-[var(--color-primary)] px-6 py-3 max-w-fit mx-auto'>Send</button>
+                  <button type="submit" className='w-fit rounded-lg text-md lg:text-2xl text-white bg-[var(--color-primary)] px-6 py-3 max-w-fit mx-auto cursor-pointer'>Send</button>
                 </div>
 
                 <div className='bg-[var(--color-primary)] text-white py-8 w-full flex flex-col justify-center items-center'>
                   <div className='flex gap-4 items-center'>
                     <img src={phoneIcon} alt="phone-icon" className='p-2 rounded-full bg-white scale-75' />
-                    <p className='text-md'>+234-904 312 0419</p>
+                    <p className='text-md'><a href="tel:+2349043120419">+234 904 312 0419</a></p>
                   </div>
                   <div className='flex gap-4 items-center'>
                     <img src={mailIcon} alt="mail-icon" className='w-10 h-10 rounded-full bg-white scale-75' />
-                    <p>hello@bigfarm.com</p>
+                    <p><a href="mailto:hello@bigfarm.com">hello@bigfarm.com</a></p>
                   </div>
                   <div className='flex gap-4 scale-50'>
                     <Link to={"javascript:void(0)"}><img src={faceBook} alt="facebook" /></Link>
@@ -106,7 +106,7 @@ Desktop version */}
 
 
             {/* desktop form  */}
-            <div className='hidden px-4 gap-8 lg:flex lg:flex-col pr-10'>
+            <form action={''} className='hidden px-4 gap-8 lg:flex lg:flex-col pr-10 '>
               <div className='space-y-1'>
                 <h2 className="text-2xl md:text-[3rem] font-semibold mt-10 lg:mt-24 lg:text-4xl">
                   Get in Touch
@@ -130,7 +130,7 @@ Desktop version */}
               </div>
 
               <button type="submit" className='cursor-pointer w-fit rounded-lg text-md font-bold text-white bg-[var(--color-primary)] px-6 py-3'>Send</button>
-            </div>
+            </form>
 
           </div>
         </div>
