aa45a668a6ead6f4f2d319d18e5bc3143846e99f
SirJaey <smartjoshua98@gmail.com>
2025-08-05 13:22:15 +0100

    fixing images on hosted link

--- src/lib/reviewers.js
+++ src/lib/reviewers.js
@@ -1,10 +1,16 @@
+import bisi from "/src/assets/images/review-bisi.jpg";
+import ifeanyi from "/src/assets/images/review-ifeanyi.jpg";
+import amina from "/src/assets/images/review-amina.jpg";
+import samuel from "/src/assets/images/review-samuel.jpg";
+import joy from "/src/assets/images/review-joy.jpg";
+
 export const reviewers = [
     {
         id: 1,
         name: "Bisi",
         review: "With BigFarma’s investment support, I was able to grow my poultry business.I got funding without the stress of bank loans.",
         additionalDetails: "Poultry Farmer, Osun",
-        profileImage: "/src/assets/images/review-bisi.jpg"
+        profileImage: bisi
     },
 
     {
@@ -12,7 +18,7 @@ export const reviewers = [
         name: "Ifeanyi",
         review: "Buying farm produce through BigFarma is fast and easy.I get good prices, and I know real farmers benefit directly from every order.",
         additionalDetails: "Buyer, Portharcourt",
-        profileImage: "/src/assets/images/review-ifeanyi.jpg"
+        profileImage: ifeanyi
     },
 
     {
@@ -20,7 +26,7 @@ export const reviewers = [
         name: "Amina",
         review: "Since I joined BigFarma, selling my crops has been easier. I now sell directly to buyers and avoid losing money.",
         additionalDetails: "Crop Farmer, Benue",
-        profileImage: "/src/assets/images/review-amina.jpg"
+        profileImage: amina
     },
 
     {
@@ -28,7 +34,7 @@ export const reviewers = [
         name: "Samuel",
         review: "Since I started using BigFarma, managing my fish farm has been much easier.I now get updates and connect directly to buyers.",
         additionalDetails: "Fish Farmer, Delta",
-        profileImage: "/src/assets/images/review-samuel.jpg"
+        profileImage: samuel
     },
 
     {
@@ -36,6 +42,6 @@ export const reviewers = [
         name: "Joy",
         review: "I love buying fresh produce directly from farmers on BigFarma. It’s easy to order, and I trust the quality every time.",
         additionalDetails: "Buyer, Lagos",
-        profileImage: "/src/assets/images/review-joy.jpg"
+        profileImage: joy
     },
 ]
