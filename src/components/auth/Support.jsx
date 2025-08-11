9dcc11dc605a5d9c3437d10122bf5ddca04d425c
SirJaey <smartjoshua98@gmail.com>
2025-08-11 15:27:42 +0100

    quick-fixes

--- src/components/auth/Support.jsx
+++ src/components/auth/Support.jsx
@@ -2,7 +2,7 @@
 import { motion, AnimatePresence } from "framer-motion";
 import { X } from "lucide-react";
 import { Link } from 'react-router-dom';
-import '../../app.css'
+import '../../App.css'
 
 const Support = ({ show, onClose }) => {
   return (
