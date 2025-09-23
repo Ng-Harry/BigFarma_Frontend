a7d0da67414b7ce5da5dfe9c55e42a1a2c5a6df8
Ng-Harry <ngereharry5@gmail.com>
2025-09-23 22:54:06 +0100

    resolve refresh issue

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,9 @@ const __dirname = path.dirname(__filename);
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    historyApiFallback: true, // 👈 important for React Router
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
