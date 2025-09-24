74d4a8670ed1e9562d3020cad71e70e75d527812
hannahonthecloud-dot <tizzergold001@gmail.com>
2025-09-24 17:32:34 +0100

    Update GroupBuyPage.jsx

--- src/pages/GroupBuyPage.jsx
+++ src/pages/GroupBuyPage.jsx
@@ -1,5 +1,5 @@
 import { Group } from "lucide-react"
-import GroupBuy from  "../components/dashboard/GroupBuy/"
+import GroupBuy from  "../components/dashboard/GroupBuy/Index.jsx"
 import React from 'react'
 
 const GroupBuyPage = () => {
