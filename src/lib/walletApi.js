41bc4e2cfe2b3e5c82e94b727b53abe126af4f96
Iranloye Hannah <iranloye8.hannah@gmail.com>
2025-10-11 14:39:59 +0100

    transaction endpoint implemented

--- src/lib/walletApi.js
+++ src/lib/walletApi.js
@@ -1,24 +1,13 @@
 import { axios } from "../lib/axios";
-// import { endpoints } from "../components/config/endpoints";
 import Cookies from "js-cookie";
+// import { transactions } from "./Transaction";
 
 
 const BASE_URL = "https://bigfarma-backend.onrender.com/api/v1";
 
-// export const walletApi = async () => {
-//     const token = Cookies.get("BIGFARMA_ACCESS_TOKEN");
-//     const res = await axios.get(`${BASE_URL}/wallet/dashboard`, {
-//         headers: {
-//             Authorization: `Bearer ${token}`,
-//             "Content-Type": "application/json",
-//         },
-//     });
-//     return res.data;
-// }
 export const walletApi = async () => {
   try {
     const token = Cookies.get("BIGFARMA_ACCESS_TOKEN");
-    console.log("Token:", token);
 
     const res = await axios.get(`${BASE_URL}/wallet/dashboard`, {
       headers: {
@@ -26,6 +15,7 @@ export const walletApi = async () => {
         "Content-Type": "application/json",
       },
     });
+ 
 
     console.log("Wallet response:", res.data);
     return res.data;
